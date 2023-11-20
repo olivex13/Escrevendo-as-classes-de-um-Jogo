df88692ca682e943eca15dc7208159a659ede58c
Douglas Silva <silva.o.douglas@gmail.com>
2023-11-20 00:48:56 -0300

    Código do desafio + alteração no readme

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-console.log("hello word")
+class heroi {
+    constructor(nome, idade, tipo){
+    this.nome = nome
+    this.idade = idade
+    this.tipo = tipo
+    }
+atacar () {
+    let ataque
+    switch (this.tipo) {
+        case 'mago':
+            ataque = "magia"
+            break;
+        case 'guerreiro':
+            ataque = "espada"
+            break
+        case 'monge':
+            ataque = "artes marciais"
+            break
+        case 'ninja':
+            ataque = "shuriken"
+            break
+        default:
+            ataque = "ataque generico"
+            break;
+    }
+    console.log(`O ${this.tipo} atacou usando ${ataque}`)
+    }
+}
+
+const heroi1 = new heroi('Cesar', 30, 'guerreiro');
+heroi1.atacar();
+
+const heroi2 = new heroi('Ronald', 50, 'mago');
+heroi2.atacar();
+
+const heroi3 = new heroi('Elias', 40, 'ninja');
+heroi3.atacar();
+
+const heroi4 = new heroi('Junior', 60, 'monge');
+heroi4.atacar();
--- readme.md
+++ readme.md
@@ -29,12 +29,17 @@ Crie uma classe generica que represente um herói de uma aventura e que possua a
 além disso, deve ter um método chamado atacar que deve atender os seguientes requisitos:
 
 - exibir a mensagem: "o {tipo} atacou usando {ataque}")
+
 - aonde o {tipo} deve ser concatenando o tipo que está na propriedade da classe
+
 - e no {ataque} deve seguir uma descrição diferente conforme o tipo, seguindo a tabela abaixo:
 
 se mago -> no ataque exibir (usou magia)
+
 se guerreiro -> no ataque exibir (usou espada)
+
 se monge -> no ataque exibir (usou artes marciais)
+
 se ninja -> no ataque exibir (usou shuriken)
 
 ## Saída
